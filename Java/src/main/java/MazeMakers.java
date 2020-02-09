1cb5cf643ccccbe9350543e2d118a07abdb849cb
RakNoel <Oskar.leir@gmail.com>
2020-02-09 01:21:17 +0100

    Finish mazemaker

--- Java/src/main/java/MazeMakers.java
+++ Java/src/main/java/MazeMakers.java
@@ -5,17 +5,19 @@ public class MazeMakers {
     static final int[] cx = {-1, 0, +1, 0};
 
     static int foundEdge = 0;
-    static boolean loops;
+    static boolean loops = false;
 
     public static void main(String[] args) {
         Kattio kattio = new Kattio(System.in, System.out);
 
         while (true) {
+            foundEdge = 0;
+            loops = false;
             int height = kattio.getInt();
             int length = kattio.getInt();
 
             //Should exit on 0 0
-            if (length == 0 && height == 0)
+            if (length == 0 || height == 0)
                 break;
 
             char[][] inn = new char[height][length];
@@ -29,21 +31,36 @@ public class MazeMakers {
                 for (int j = 0; j < length; j++)
                     maze[i][j] = Integer.parseInt(inn[i][j] + "", 16);
 
-            int root = -1;
-            int sy = 0;
-            int sx = 0;
-            for (int i = 0; i < height && root < 0; i++)
-                for (int j = 0; j < length; j++)
-                    if (inn[i][j] != 'F') {
-                        root = maze[i][j];
-                        sx = j;
-                        sy = i;
-                        break;
-                    }
+            int sy = -1;
+            int sx = -1;
+
+            //Find any entrance on any side
+            for (int i = 0; i < length && sx < 0 && sy < 0; i++) {
+                var tmpTop = maze[0][i];
+                var tmpBot = maze[height - 1][i];
+                if ((tmpTop & 8L) == 0) {
+                    sx = i;
+                    sy = 0;
+                } else if ((tmpBot & 2L) == 0){
+                    sx = i;
+                    sy = height - 1;
+                }
+            }
+            for (int i = 0; i < height && sx < 0 && sy < 0; i++) {
+                var tmpLeft = maze[i][0];
+                var tmpRight = maze[i][length - 1];
+                if ((tmpRight & 4L) == 0) {
+                    sx = length - 1;
+                    sy = i;
+                } else if ((tmpLeft & 1L) == 0){
+                    sx = 0;
+                    sy = i;
+                }
+            }
 
             dfs(sx, sy, visited, maze);
 
-            if (foundEdge < 2) {
+            if (foundEdge != 2) {
                 System.out.println("NO SOLUTION");
             } else if (visited.cardinality() != height * length) {
                 System.out.println("UNREACHABLE CELL");
@@ -59,7 +76,6 @@ public class MazeMakers {
         visited.set(_x, _y, true);
         var hold = graph[_y][_x];
         int alreadyVisited = 0;
-        //System.out.printf("x:%d y:%d, value:%d, cell:%s %n", _x, _y, hold, Integer.toHexString(hold));
 
         for (int m = 1, i = 0; m <= 8; m *= 2, i++)
             if ((hold & m) == 0) {
